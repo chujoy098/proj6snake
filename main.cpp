ff3602bde4f25ce4c9102362b78e23242a961f3e
chujoy098 <chujoy098@gmail.com>
2026-02-11 22:08:11 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,8 +2,8 @@
 #include <chrono>
 #include <thread>
 #include <stdlib.h>
-#include "include/main.h"
-#include <random>
+#include <pthread.h>
+#include "main.h"
 
 int main()
 {
@@ -11,7 +11,7 @@ int main()
     int score = 0;
     bool gameoflag = false;
     std::cout << "snakegame."<< std::endl;
-    unsigned char** im = (unsigned char**)malloc(25 * sizeof(unsigned char*));
+    int im[40][25];
     int foodx = rand()%40, foody = rand()%25;
     int x = 20, y = 13;
     for (int i = 0; i<40; i++)
@@ -20,22 +20,23 @@ int main()
         {
             if (i == foodx && j == foody)
             {
-                im[i][j] = 'O';
+                im[i][j] = 1;
             }
             else
             {
-                im[i][j] = ' ';
+                im[i][j] = 0;
             }
         }
     }
     /*[x][y], 25 = y 40 = x*/
-    int TailX[50], TailY[50], len = 1;
+    int TailX[40], TailY[40], len = 1;
+    GameRender(im, score, x, y, len, TailX, TailY, 40, 25);
     do{
-        
+        UserInput;
         NextFrame(x, y, Dir, len, TailX, TailY, score, im);
-
+        GameRender(im, score, x, y, len, TailX, TailY, 40, 25);
         gameoflag = GameOver(x, y, len, TailX, TailY, 40, 25, im);
-        std::this_thread::sleep_for(std::chrono::milliseconds(300));
-    }while(gameoflag = false);
+        std::this_thread::sleep_for(std::chrono::milliseconds(600));
+    }while(gameoflag == false);
     return 0;
 }
